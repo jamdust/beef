5ca573c70dfb966c9a15d94e68f5f8b0573860ff
root <root@leal-rhel7.devlnk.net>
2017-11-10 15:13:29 -0800

    committing zeroes tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
+import {AppComponent} from './app.component';
+import {ZeroesComponent} from './zeroes/zeroes.component';
+import {ZeroDetailComponent} from './zero-detail/zero-detail.component';
 
-import { AppComponent } from './app.component';
-
+import {ZeroService} from './zero.service';
+import {MessagesComponent} from './messages/messages.component';
+import {MessageService} from './message.service';
+import {AppRoutingModule} from './app-routing.module';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ZeroesComponent,
+    ZeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [ZeroService, MessageService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
